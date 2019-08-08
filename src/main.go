67bfce8a116dafe1c881e1e29e730456b599172a
Wil Faver <wilfa4243@hotmail.com>
2019-08-08 11:35:43 -0500

    Nuevos cambios

--- src/main.go
+++ src/main.go
@@ -4,6 +4,7 @@ import (
 	
 	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 	"github.com/gin-gonic/gin"
 	_"github.com/go-sql-driver/mysql"
@@ -20,6 +21,34 @@ type Employee struct {
 func defineEndpointsAndRunApp() {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
+
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"greet": "hello, world!",
+		})
+	})
+
+	r.GET("/echo/:echo", func(c *gin.Context) {
+		echo := c.Param("echo")
+		c.JSON(http.StatusOK, gin.H{
+			"echo": echo,
+		})
+	})
+
+	r.POST("/upload", func(c *gin.Context) {
+		form, _ := c.MultipartForm()
+		files := form.File["upload[]"]
+
+		for _, file := range files {
+			log.Println(file.Filename)
+
+			// Upload the file to specific dst.
+			// c.SaveUploadedFile(file, dst)
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"uploaded": len(files),
+		})
+	})
 
 	r.GET("/employees", GetEmployees)
 	r.Run() // listen and serve on 0.0.0.0:8080
